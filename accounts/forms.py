769d411a5845d7eabb644ff1b50d0aba8384dd4e
..:: HamdiL ::.. <hamdilhasan200@gmail.com>
2025-10-19 00:27:26 +0600

    Add user registration and profile forms

---
+++ accounts/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['age', 'mobile', 'address', 'gender', 'emergency_contact', 'is_field_owner', 'profile_picture']
+        widgets = {
+            'age': forms.NumberInput(attrs={'class': 'form-control', 'min': '1', 'max': '120', 'value': '18'}),
+            'mobile': forms.TextInput(attrs={'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'gender': forms.Select(attrs={'class': 'form-control'}),
+            'emergency_contact': forms.TextInput(attrs={'class': 'form-control'}),
+            'profile_picture': forms.FileInput(attrs={'class': 'form-control'}),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not self.instance.pk and not self.initial.get('age'):
+            self.initial['age'] = 18
+        if not self.instance.pk and not self.initial.get('gender'):
+            self.initial['gender'] = 'Male'
+
