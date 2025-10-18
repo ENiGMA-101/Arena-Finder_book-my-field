8ab69e7f2e3ab259489972b1ee48e0132901ad5e
..:: HamdiL ::.. <hamdilhasan200@gmail.com>
2025-10-19 00:28:02 +0600

    Add user update and password reset forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -35,3 +35,82 @@ class UserProfileForm(forms.ModelForm):
         if not self.instance.pk and not self.initial.get('gender'):
             self.initial['gender'] = 'Male'
 
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+class ForgotPasswordForm(forms.Form):
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your username',
+            'required': True
+        }),
+        help_text='We will verify if this account exists in our system'
+    )
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if not username:
+            raise forms.ValidationError("Please enter your username.")
+        
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise forms.ValidationError("Sorry, we couldn't find an account with that username.")
+        
+        return username
+
+class ResetPasswordForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'readonly': True,
+            'style': 'background-color: #f8f9fa;'
+        })
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'readonly': True,
+            'style': 'background-color: #f8f9fa;'
+        })
+    )
+    new_password = forms.CharField(
+        label='New Password',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Create a strong password',
+            'minlength': '6'
+        }),
+        help_text='Password should be at least 6 characters long'
+    )
+    confirm_password = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Type your password again'
+        })
+    )
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('new_password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        if password and confirm_password:
+            if password != confirm_password:
+                raise forms.ValidationError("The passwords you entered don't match. Please try again.")
+            
+            if len(password) < 6:
+                raise forms.ValidationError("Password is too short. Please use at least 6 characters.")
+        
+        return cleaned_data
