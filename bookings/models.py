7119ac93b6c07b0a9f6f666c41cb31082c8120e3
TOUSHIF55 <140740267+TOUSHIF55@users.noreply.github.com>
2025-10-20 00:16:22 +0600

    upd: bookings app done

--- bookings/models.py
+++ bookings/models.py
@@ -94,7 +94,7 @@ class Payment(models.Model):
     payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
     mobile_number = models.CharField(max_length=15)
     transaction_id = models.CharField(max_length=50, unique=True)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'))  # Added default
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00')) 
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
